41dcb401e305a1cc53395393352da26a2003401b
kunaalkumar <kunaal@ashiana.com>
2019-02-04 00:58:39 -0700

    Request for additional information via API call Update DTO to handle extra information

--- app/src/main/java/com/kunaalkumar/suggsn/taste_dive/DTO.kt
+++ app/src/main/java/com/kunaalkumar/suggsn/taste_dive/DTO.kt
@@ -2,5 +2,6 @@ package com.kunaalkumar.suggsn.taste_dive
 
 data class TDResult(val Similar: TDSimilar)
 data class TDSimilar(val Info: List<TDItem>, val Result: List<TDItem>)
-data class TDItem(val Name: String, val Type: String)
+data class TDItem(val Name: String, val Type: String, val wTeaser: String, val wUrl: String,
+                  val yUrl: String)
 
--- app/src/main/java/com/kunaalkumar/suggsn/taste_dive/ITasteDiveService.kt
+++ app/src/main/java/com/kunaalkumar/suggsn/taste_dive/ITasteDiveService.kt
@@ -4,8 +4,11 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * Reference: https://tastedive.com/read/api
+ */
 interface ITasteDiveService {
 
-    @GET("similar/type=movies")
+    @GET("similar/type=movies&info=1")
     fun getSimilarMovies(@Query("api_key") k: String) : Call<TDResult>
 }
