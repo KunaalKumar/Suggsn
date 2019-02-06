772818a8f7d8c07d462587e53dae9b2255f88573
kunaalkumar <kunaal@ashiana.com>
2019-02-06 02:59:10 -0700

    Fix problem with navigation component and fragment Update search result view Update theme to not include action bar

--- app/src/main/java/com/kunaalkumar/suggsn/ui/SearchResults.kt
+++ app/src/main/java/com/kunaalkumar/suggsn/ui/SearchResults.kt
@@ -1,38 +1,47 @@
 package com.kunaalkumar.suggsn.ui
 
+
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.kunaalkumar.suggsn.R
 import com.kunaalkumar.suggsn.taste_dive.RetrofitFactory
+import kotlinx.android.synthetic.main.fragment_search_results.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 /**
  * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SearchResults.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SearchResults.newInstance] factory method to
- * create an instance of this fragment.
  *
  */
 class SearchResults : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-        val service = RetrofitFactory.makeRetrofitService()
-        GlobalScope.launch(Dispatchers.Main) {
-            val request = service.getSimilarMovies("Drive")
-            try {
-                val response = request.await()
-                Toast.makeText(context, response.body()!!.Similar.Results[0].Name, Toast.LENGTH_LONG).show()
-            } catch (e: Throwable) {
-                Toast.makeText(context, "Something went wrong", Toast.LENGTH_LONG).show()
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_results, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        search_button.setOnClickListener {
+            val service = RetrofitFactory.makeRetrofitService()
+            GlobalScope.launch(Dispatchers.Main) {
+                val request = service.getSimilarMovies(search_edit_text.text.toString())
+                try {
+                    val response = request.await()
+                    Toast.makeText(context, response.body()!!.Similar.Results[0].Name, Toast.LENGTH_LONG).show()
+                } catch (e: Throwable) {
+                    Toast.makeText(context, "Something went wrong", Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
